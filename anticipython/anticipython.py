2e37aadc8996c71abe04c9a7c3ac521bfb1c9c93
Brian Cristante <brian.cristante@gmail.com>
2019-08-15 22:02:08 -0400

    Download PEPs from python.org

--- anticipython/anticipython.py
+++ anticipython/anticipython.py
@@ -1,6 +1,10 @@
+import sys
 from datetime import datetime
 
-from peps_store import PepsStore
+import requests
+
+def error(message):
+    print(message, file=sys.stderr)
 
 def _download(pep_number):
     """
@@ -9,23 +13,32 @@ def _download(pep_number):
     # PEP URLs look like https://www.python.org/dev/peps/pep-0123/
     formatted_pep_number = str(pep_number).rjust(4, '0')
     url = f'https://www.python.org/dev/peps/pep-{formatted_pep_number}/'
-    return None
+
+    print(f'Downloading PEP {pep_number} ...')
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print('Done.')
+        return response.text
+    else:
+        error(f'Could not download {url}. Status code: {response.status_code}')
+        return None
 
 def _scrape_releases(pep_html):
     """
     Find the Python versions and their corresponding release dates
     in a PEP for a release schedule.
     """
-    return 'mock-version', datetime.now()
+    yield 'mock-version-1', datetime.now()
+    yield 'mock-version-2', datetime.now()
 
-def get_release_dates(peps):
+def get_release_dates(peps, peps_store):
     """
     Create a mapping of Python versions to their release dates.
     """
-    peps_store = PepsStore('.anticipython_peps_store')
     for version, pep_number in peps.items():
         html = peps_store.load_pep(pep_number)
         if html is None:
             html = _download(pep_number)
             peps_store.store_pep(pep_number, html)
-        yield _scrape_releases(html)
+        yield from _scrape_releases(html)
--- anticipython/main.py
+++ anticipython/main.py
@@ -2,9 +2,11 @@ import sys
 
 import anticipython
 import peps
+import peps_store
 
 def main(argv):
-    release_dates = anticipython.get_release_dates(peps.peps)
+    store = peps_store.PepsStore('./.anticipython_peps_store/')
+    release_dates = anticipython.get_release_dates(peps.peps, store)
     for version, date in release_dates:
         print(f'{version}: {date}')
 
