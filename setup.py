29395119e7a2e51ec8ed93fb24ec3619388c8527
Brian Cristante <brian.cristante@gmail.com>
2019-08-16 22:52:55 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setup(
+    name='anticipython',
+    version='0.1.0',
+    description='Create .ics calendars for upcoming CPython releases 🐍👀',
+    packages=['anticipython'],
+    python_requires='>=3.6',
+    url='https://github.com/brcrista/Anticipython',
+    author='Brian Cristante',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+)
